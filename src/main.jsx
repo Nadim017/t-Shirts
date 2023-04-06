9da22aaf074939cd5dabac5810c6873174131229
nadim <mdnadimmahamud704@gmail.com>
2023-04-07 01:25:19 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './Component/Home/Home';
+import OrderReview from './Component/OrderReview/OrderReview';
+import Main from './Component/Layout/Main';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+        loader: () => fetch('data.json'),
+      },
+      { path: 'review', element: <OrderReview></OrderReview> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
